7e169adfae31a59ff91e343b902cfa611997e681
David Herron <david@davidherron.com>
2019-10-07 21:16:12 -0700

    Use @akashacms/plugins-booknav

--- config.js
+++ config.js
@@ -32,7 +32,7 @@ config
         generateSitemapFlag: true
     })
     .use(require('akashacms-breadcrumbs'))
-    .use(require('akashacms-booknav'))
+    .use(require('@akashacms/plugins-booknav'))
     .use(require('akashacms-embeddables'))
     .use(require('akashacms-blog-podcast'));
 
