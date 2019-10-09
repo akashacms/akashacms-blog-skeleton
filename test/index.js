1eb83d63d77c88b0ea23c1e312e80f6be2108fc2
David Herron <david@davidherron.com>
2019-10-08 23:23:02 -0700

    Update test to use @akashacms/plugins-blog-podcast

--- test/index.js
+++ test/index.js
@@ -195,8 +195,8 @@ describe('check pages', function() {
 
 describe('documents and index', function() {
     it('should have correct documents', async function() {
-        let blogcfg = config.plugin('akashacms-blog-podcast').options.bloglist['news'];
-        let documents = await config.plugin('akashacms-blog-podcast').findBlogDocs(config, blogcfg);
+        let blogcfg = config.plugin('@akashacms/plugins-blog-podcast').options.bloglist['news'];
+        let documents = await config.plugin('@akashacms/plugins-blog-podcast').findBlogDocs(config, blogcfg);
         assert.equal(documents.length, 4);
         assert.equal(documents[0].docpath, 'blog/2015/11/test-post-2.html.md');
         assert.equal(documents[1].docpath, 'blog/2015/11/test-post-1.html.md');
@@ -205,8 +205,8 @@ describe('documents and index', function() {
     });
 
     it('should have correct index', async function() {
-        let blogcfg = config.plugin('akashacms-blog-podcast').options.bloglist['news'];
-        let indexes = await config.plugin('akashacms-blog-podcast').findBlogIndexes(config, blogcfg);
+        let blogcfg = config.plugin('@akashacms/plugins-blog-podcast').options.bloglist['news'];
+        let indexes = await config.plugin('@akashacms/plugins-blog-podcast').findBlogIndexes(config, blogcfg);
         assert.equal(indexes.length, 0);
     });
 });
